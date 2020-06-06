287ffa4ca24cc9def324fabd6b1d3238f9693d49
lidium <elesahich@gmail.com>
2020-06-07 01:41:49 +0900

    Revert "(임시커밋)"
    This reverts commit eebed239535a56cac4dde2a4afb08996a59d8eba.

---
+++ CinemaSalon/CinemaSalon/Scene/MainHome/View/ViewControllers/ViewController.swift
@@ -0,0 +1,24 @@
+//
+//  ViewController.swift
+//  CinemaSalon
+//
+//  Created by elesahich on 2020/06/06.
+//  Copyright © 2020 elesahich. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+<<<<<<< HEAD:CinemaSalon/CinemaSalon/Scene/MainHome/View/ViewControllers/MainVC.swift
+        
+=======
+        // Do any additional setup after loading the view.
+>>>>>>> parent of eebed23... (임시커밋):CinemaSalon/CinemaSalon/Scene/MainHome/View/ViewControllers/ViewController.swift
+    }
+
+
+}
+
